ad1b08935388c86c8f7628caa1035d5874bf5e5b
hoachnt <hoachprogrammer@gmail.com>
2024-07-16 15:08:53 +0700

    added dialog to confirm delete kanban item

--- src/app/ui/drop-list/drop-list.component.ts
+++ src/app/ui/drop-list/drop-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, inject, Input, Output } from "@angular/core";
+import {
+    ChangeDetectionStrategy,
+    Component,
+    EventEmitter,
+    inject,
+    Input,
+    Output,
+    signal,
+} from "@angular/core";
 import { CdkDragDrop, CdkDrag, CdkDropList } from "@angular/cdk/drag-drop";
 import { DropItemSortPipe } from "../../helpers/pipes/drop-item-sort.pipe";
 import { IKanbanItem } from "../../libraries/directus/directus";
@@ -8,6 +16,17 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { KanbanService } from "../../data/services/kanban.service";
 import { firstValueFrom } from "rxjs";
+import {
+    MAT_DIALOG_DATA,
+    MatDialog,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogRef,
+    MatDialogTitle,
+} from "@angular/material/dialog";
+
+interface DialogData extends IKanbanItem {}
 
 @Component({
     selector: "app-drop-list",
@@ -23,6 +42,7 @@ import { firstValueFrom } from "rxjs";
     ],
     templateUrl: "./drop-list.component.html",
     styleUrl: "./drop-list.component.scss",
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DropListComponent {
     @Input() data!: IKanbanItem[] | null;
@@ -32,18 +52,55 @@ export class DropListComponent {
     @Input() dropListId!: number | undefined;
     @Output() itemDropped = new EventEmitter<CdkDragDrop<IKanbanItem[]>>();
 
-    kanbanService = inject(KanbanService);
-
-    isDisabled = true;
+    readonly dialog = inject(MatDialog);
 
     drop(event: CdkDragDrop<IKanbanItem[]>) {
         this.itemDropped.emit(event);
     }
 
-    async onDeleteKanbanItem(kanbanId: number | undefined) {
-        if (kanbanId === undefined) return;
+    openDialog(kanbanItem: DialogData): void {
+        if (this.data === null) return;
 
-        await firstValueFrom(this.kanbanService.deleteKanbanItem(kanbanId));
+        const dialogRef = this.dialog.open(DialogDeleteKanbanItem, {
+            data: { title: kanbanItem.title, id: kanbanItem.id },
+        });
+
+        dialogRef.afterClosed().subscribe();
+    }
+}
+
+@Component({
+    selector: "dialog-delete-kanban-item",
+    templateUrl: "dialog-delete-kanban-item.component.html",
+    standalone: true,
+    imports: [
+        MatButtonModule,
+        MatDialogTitle,
+        MatDialogContent,
+        MatDialogActions,
+        MatDialogClose,
+    ],
+})
+export class DialogDeleteKanbanItem {
+    readonly dialogRef = inject(MatDialogRef<DialogDeleteKanbanItem>);
+    readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+
+    readonly kanbanService = inject(KanbanService);
+    readonly isDeleting = signal(false);
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
+    async onDeleteKanbanItem() {
+        if (this.data.id === undefined) return;
+
+        this.isDeleting.set(true);
+
+        await firstValueFrom(this.kanbanService.deleteKanbanItem(this.data.id));
         await firstValueFrom(this.kanbanService.getKanbanItems());
+
+        this.dialogRef.close();
+        this.isDeleting.set(false);
     }
 }
