b683fc0d0d69b0b8c310df27d34da88cc81c0075
hoachnt <hoachprogrammer@gmail.com>
2024-07-25 18:16:32 +0700

    Updated dependencies and added ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,23 +3,27 @@ import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
 import {
     MAT_DATE_LOCALE,
     provideNativeDateAdapter,
 } from "@angular/material/core";
 import { authTokenInterceptor } from "./auth/auth.interceptor";
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration } from "@angular/platform-browser";
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideAnimationsAsync(),
-        provideHttpClient(withInterceptors([authTokenInterceptor])),
+        provideHttpClient(
+            withInterceptors([authTokenInterceptor]),
+            withFetch()
+        ),
         provideAnimationsAsync(),
         provideNativeDateAdapter(),
 
-        { provide: MAT_DATE_LOCALE, useValue: "vi-VN" }, provideClientHydration(),
+        { provide: MAT_DATE_LOCALE, useValue: "vi-VN" },
+        provideClientHydration(),
     ],
 };
